afdf112ba63da2acaa64809d6a8ca4c01ded45c1
Fahd <36855117+fqhd@users.noreply.github.com>
2023-01-10 22:58:33 +0000

    fix: improve password security

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const PASSWORD_LENGTH = 16;
+const PASSWORD_LENGTH = 24;
 
 const randomArray = new Uint8Array(PASSWORD_LENGTH);
 
@@ -9,8 +9,7 @@ let password = "";
 const validChars = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-  '1', '2', '3', '4', '5', '6', '7', '8', '9',
-  '_', '.', '/', '$', '(', ')', '"', '&', '<', '>', '?'
+  '1', '2', '3', '4', '5', '6', '7', '8', '9'
 ];
 
 for(let i = 0; i < PASSWORD_LENGTH; i++){
